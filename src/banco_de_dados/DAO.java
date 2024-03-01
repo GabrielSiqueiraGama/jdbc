65cbc6c15e430046bb97ac4d13213e35938a5acf
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-01 10:32:09 -0300

    feat: Criando DAO para encapsulamento o processo de inclusão de SQL.

---
+++ src/banco_de_dados/DAO.java
@@ -0,0 +1,56 @@
+package banco_de_dados;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DAO {
+
+	private Connection conexao;
+	
+	public int incluir(String sql, Object... atributos) {
+		try {
+			PreparedStatement statement = getConexao().prepareStatement(
+					sql, PreparedStatement.RETURN_GENERATED_KEYS);
+			adicionarAtributos(statement, atributos);
+			if(statement.executeUpdate() > 0) {
+				ResultSet resultado = statement.getGeneratedKeys();
+					if(resultado.next()) {
+						return resultado.getInt(1);
+					}
+			}
+			return -1;
+		}catch(SQLException e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private void adicionarAtributos(PreparedStatement statement,
+			Object[] atributos) throws SQLException{
+		
+		int indice = 1;
+		for(Object atributo: atributos) {
+			if(atributo instanceof String) {
+				statement.setString(indice, (String) atributo);
+			}else if(atributo instanceof Integer) {
+				statement.setInt(indice, (Integer) atributo);
+			}
+			indice++;
+		}
+	}
+	
+	private Connection getConexao() {
+		
+		try {
+			if(conexao != null && conexao.isClosed()) {
+				return conexao;
+			}
+		} catch (SQLException e) {
+			
+		}
+		conexao = FabricaConexao.getConexao();
+		return conexao;
+	}
+	
+}
