7909d777dab6bfc7acad267bc4599c91abfea918
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-01 00:10:25 -0300

    feat: conectando repositorio ao banco de dados através do properties

--- src/banco_de_dados/FabricaConexao.java
+++ src/banco_de_dados/FabricaConexao.java
@@ -1,21 +1,32 @@
 package banco_de_dados;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
 	public static Connection getConexao() {
 		try {
-			final String url = "jdbc:mysql://localhost/estudo_java?verifyServerCertificate=false&useSSL=true";
-			final String usuario = "root";
-			final String senha = "123456789!";
+			Properties prop = getProperties();
+			
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
 			
 			return DriverManager.getConnection(url, usuario, senha);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private static Properties getProperties() throws IOException{
+		Properties prop = new Properties();
+		String caminho = "/conexao.properties";
+		prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+		return prop;
 	}
 	
 }
